667d304e9eed0fe9eab3a5be5c1ebf0158b55661
Grégory Néault <85392282+Gneo45@users.noreply.github.com>
2022-01-23 14:54:53 +0100

    v2

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(
+    .connect( process.env.MONGODB_URI ||
         "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.pg86x.mongodb.net/mern-project", {
             useNewUrlParser: true,
             useUnifiedTopology: true
