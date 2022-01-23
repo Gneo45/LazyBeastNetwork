e2dc15aa0bae587b2daced2477f59717472e7e5d
Grégory Néault <85392282+Gneo45@users.noreply.github.com>
2022-01-23 11:55:13 +0100

    connection

--- config/db.js
+++ config/db.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose
     .connect(
-        "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.pg86x.mongodb.net/mern-project", {
+        process.env.DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
     .then(() => console.log('Connected to MongoDb'))
     .catch((err) => console.log("Failed to connect to MongoDB", err));
+ 
